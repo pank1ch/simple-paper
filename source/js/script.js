b72568760ba990f71d09034ec9a2603a4ff7e40a
Kirill <kirya.kirilenko.84@mail.ru>
2024-06-15 19:52:19 +0700

    добавляет сборку и начальные стили

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+new Swiper(".brands__slider", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  updateOnWindowResize: true,
+  speed: 2500,
+  allowTouchMove: false,
+  autoplay: {
+    enabled: true,
+    delay: 1,
+  },
+
+});
